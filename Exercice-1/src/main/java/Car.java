7244bb3f5bff1729b0c203c04b016b6540585a9f
David Gageot <david@gageot.net>
2010-12-01 13:18:44 +0100

    Green tests!!!!

--- Exercice-1/src/main/java/Car.java
+++ Exercice-1/src/main/java/Car.java
@@ -1,39 +1,19 @@
 import java.util.Date;
 
-/**
- * Created by IntelliJ IDEA.
- * User: maximenowak
- * Date: 20 oct. 2010
- * Time: 13:10:28
- * To change this template use File | Settings | File Templates.
- */
 public class Car {
-    public Car(Date registrationDate, int engineSize) {
+	private final Date registrationDate;
+	private final int engineSize;
+
+	public Car(Date registrationDate, int engineSize) {
 		this.registrationDate = registrationDate;
 		this.engineSize = engineSize;
 	}
-
-	public Car(Date date, int size, int co2) {
-		// TODO Auto-generated constructor stub
-	}
-
-	private Date registrationDate;
-    private int engineSize;
 
     public int getEngineSize() {
         return engineSize;
     }
 
-    public void setEngineSize(int engineSize) {
-        this.engineSize = engineSize;
-    }
-
-
-    public void setRegistrationDate(Date date) {
-        this.registrationDate = date;
-    }
-
-    public Date getRegistrationDate() {
-        return registrationDate;
-    }
+	public boolean isRegisteredAfter(Date date) {
+		return registrationDate.compareTo(date) >= 0;
+	}
 }
--- Exercice-1/src/main/java/TaxCalculator.java
+++ Exercice-1/src/main/java/TaxCalculator.java
@@ -1,73 +1,39 @@
-import org.apache.commons.lang.time.DateUtils;
-
 import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-/**
- * Created by IntelliJ IDEA.
- * User: maximenowak
- * Date: 20 oct. 2010
- * Time: 12:27:17
- * To change this template use File | Settings | File Templates.
- */
 public class TaxCalculator {
+	protected static final Date CO2_TAX_DATE;
 
-    protected static final Date CO2_TAX_DATE;
+	static {
+		Calendar cal = new GregorianCalendar();
 
-    static {
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.MONTH, Calendar.MARCH);
+		cal.set(Calendar.YEAR, 2001);
 
-        Calendar cal = new GregorianCalendar();
+		CO2_TAX_DATE = cal.getTime();
+	}
 
-        cal.set(Calendar.DAY_OF_MONTH, 1);
-        cal.set(Calendar.MONTH, Calendar.MARCH);
-        cal.set(Calendar.YEAR, 2001);
+	public BigDecimal calculate(Van van) {
+		if (van.getWeight() < 3500) {
+			return BigDecimal.valueOf(165L);
+		}
+		return BigDecimal.valueOf(190L);
+	}
 
-        CO2_TAX_DATE = cal.getTime();
-    }
+	public BigDecimal calculate(Car car) {
+		if (isRegistrationDateAfterFirstMarch2001(car)) {
+			return BigDecimal.ZERO;
+		}
+		if (car.getEngineSize() <= 1550) {
+			return BigDecimal.valueOf(110L);
+		}
+		return BigDecimal.valueOf(165L);
+	}
 
-    public BigDecimal calculate(Van van) {
-
-        BigDecimal bd;
-
-        if (van.getWeight() < 3500) {
-
-            bd = BigDecimal.valueOf(165L);
-
-        } else {
-
-            bd = BigDecimal.valueOf(190L);
-        }
-
-        return bd;
-    }
-    public BigDecimal calculate(Car car) {
-
-        BigDecimal bd;
-
-        if (isRegistrationDateAfterFirstMarch2001(car)) {
-            bd = BigDecimal.ZERO;
-        }
-        else {
-            if (car.getEngineSize() <= 1550) {
-
-                bd = BigDecimal.valueOf(110L);
-
-            } else {
-
-                bd = BigDecimal.valueOf(165L);
-            }
-        }
-
-        return bd;
-    }
-
-
-
-    protected boolean isRegistrationDateAfterFirstMarch2001(Car car) {
-
-   
-        return car.getRegistrationDate().after(CO2_TAX_DATE);
-    }
+	protected boolean isRegistrationDateAfterFirstMarch2001(Car car) {
+		return car.isRegisteredAfter(CO2_TAX_DATE);
+	}
 }
